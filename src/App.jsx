98e346047a36d857d23125caf082e4062603aec2
ShahadatHasan623 <shahadathasan743599@gmail.com>
2026-03-03 21:11:20 +0600

    added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import './App.css'
-import Navbar from './components/Navbar/Navbar'
+import "./App.css";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-
-
   return (
-    <div className='bg-slate-100 min-h-screen'>
-     <Navbar></Navbar>
-      
+    <div className="bg-slate-100 min-h-screen">
+      <Navbar></Navbar>
+      <Home></Home>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+const Home = () => {
+  return (
+    <div className="flex max-w-7xl mx-auto gap-5 my-10">
+      <div className="card bg-linear-to-br from-[#632EE3] to-[#9F62F2] h-56 flex-1 flex justify-center items-center">
+        <h1 className="text-2xl font-semibold text-[#FFFF]">In-Progress</h1>
+        <span className="text-xl text-[#FFF] font-semibold">0</span>
+      </div>
+      <div className="card bg-linear-to-br from-[#54CF68] to-[#00827A] h-56 flex-1 flex justify-center items-center">
+        <h1 className="text-2xl font-semibold text-[#FFF]">Resolved</h1>
+        <span className="text-xl text-[#FFF] font-semibold">0</span>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
