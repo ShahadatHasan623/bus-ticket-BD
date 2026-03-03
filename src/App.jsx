31627de70aa709c7ab20808f732c7839ebdc9c94
ShahadatHasan623 <shahadathasan743599@gmail.com>
2026-03-04 00:44:53 +0600

    added conditional rendering card section status

--- src/App.jsx
+++ src/App.jsx
@@ -5,38 +5,45 @@ import Customers from "./components/Customers/Customers";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 
-const busTicketFetch = async()=>{
-  const res = await fetch('../public/bus_ticket.json')
-  return res.json()
-}
-
-const ticket =busTicketFetch()
-function App() {
-
-  const [customers,setCustomers] =useState([])
-  const [resolved,setResolved] =useState([])
-
- const handleResolvedTask = (task) => {
-
-  const remainingCustomers = customers.filter(
-    (item) => item.id !== task.id
-  );
-  setCustomers(remainingCustomers);
-  const newResolved = [...resolved, task];
-  setResolved(newResolved);
+const busTicketFetch = async () => {
+  const res = await fetch("../public/bus_ticket.json");
+  return res.json();
 };
+
+const ticket = busTicketFetch();
+function App() {
+  const [customers, setCustomers] = useState([]);
+  const [resolved, setResolved] = useState([]);
+
+  const handleResolvedTask = (task) => {
+    const remainingCustomers = customers.filter((item) => item.id.split("-")[1] !== task.id.split("-")[1]);
+    setCustomers(remainingCustomers);
+    const newResolved = [...resolved, task];
+    setResolved(newResolved);
+  };
+
   return (
     <div className="bg-slate-100 min-h-screen">
       <Navbar></Navbar>
       <Home customers={customers} resolved={resolved}></Home>
       <div className="grid grid-cols-1 md:grid-cols-12 gap-5 max-w-7xl mx-auto">
         <div className="md:col-span-9 ">
-          <Suspense fallback={<span className="loading loading-dots loading-xl"></span>}>
-             <Customers customers={customers} setCustomers={setCustomers} ticket={ticket}></Customers>
+          <Suspense
+            fallback={<span className="loading loading-dots loading-xl"></span>}
+          >
+            <Customers
+              customers={customers}
+              setCustomers={setCustomers}
+              ticket={ticket}
+            ></Customers>
           </Suspense>
         </div>
-        <div className="md:col-span-3">
-          <Booking handleResolvedTask={handleResolvedTask} customers={customers} resolved={resolved}></Booking>
+        <div className="md:col-span-3 md:px-0 px-5">
+          <Booking
+            handleResolvedTask={handleResolvedTask}
+            customers={customers}
+            resolved={resolved}
+          ></Booking>
         </div>
       </div>
     </div>
--- src/components/Customer/Customer.jsx
+++ src/components/Customer/Customer.jsx
@@ -1,39 +1,49 @@
 import React from "react";
 import { BiCalendar } from "react-icons/bi";
 
+const Customer = ({ ticket, handleAddTask }) => {
+  const { id, title, description, customer, priority, status, createdAt } =
+    ticket;
+  const priorityColor =
+    ticket.priority === "High" || ticket.priority === "Critical"
+      ? "text-red-500"
+      : "text-green-500";
 
-const Customer = ({ ticket,handleAddTask }) => {
-   const {id,title,description,customer,priority,status,createdAt}=ticket
-   const priorityColor = ticket.priority === 'High' || ticket.priority === 'Critical' 
-    ? 'text-red-500' 
-    : 'text-gray-500';
+    const statusColor =
+      ticket.status === "Open" ? "flex items-center gap-1.5 bg-green-100 text-green-700 px-3 py-1 rounded-full text-sm font-medium" : "flex items-center gap-1.5 bg-amber-100 text-amber-700 px-3 py-1 rounded-full text-sm font-medium"
+    
   return (
-    <div onClick={()=>handleAddTask(ticket)} className=" bg-white rounded-xl shadow-sm border border-gray-100 p-5 font-sans">
+    <div
+      onClick={() => {
+        handleAddTask(ticket);
+      }}
+      className=" bg-white rounded-xl shadow-sm border border-gray-100 p-5 font-sans"
+    >
       {/* Header Section: Title and Status */}
       <div className="flex justify-between items-start mb-3">
         <h3 className="text-lg font-bold text-slate-800 leading-tight">
           {title}
         </h3>
-        <span className="flex items-center gap-1.5 bg-green-100 text-green-700 px-3 py-1 rounded-full text-sm font-medium">
-          <span className="w-2 h-2 bg-green-600 rounded-full"></span>
+        <span className={`${statusColor}`}>
+          <span className={`w-2 h-2 rounded-full ${ticket.status ==="In Progress"? "bg-amber-600":"bg-green-600"}`}></span>
           {status}
         </span>
       </div>
 
       {/* Description */}
-      <p className="text-gray-500 text-sm mb-6 line-clamp-2">
-        {description}
-      </p>
+      <p className="text-gray-500 text-sm mb-6 line-clamp-2">{description}</p>
 
       {/* Footer Section: ID, Priority, Customer, and Date */}
       <div className="flex items-center justify-between border-t pt-4 text-sm text-gray-500">
         <div className="flex gap-4 items-center">
-          <span className="font-medium">#{ticket.id.split('-')[1] || id}</span>
-          <span className={`font-bold uppercase tracking-wide text-xs ${priorityColor}`}>
+          <span className="font-medium">#{ticket.id.split("-")[1] || id}</span>
+          <span
+            className={`font-bold uppercase tracking-wide text-xs ${priorityColor}`}
+          >
             {priority} PRIORITY
           </span>
         </div>
-        
+
         <div className="flex items-center gap-4">
           <span className="text-slate-700">{customer}</span>
           <div className="flex items-center gap-1.5">
