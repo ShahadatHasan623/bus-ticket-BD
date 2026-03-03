1cf4336327104c5e2bc02a50f7dd017c15f75dd3
ShahadatHasan623 <shahadathasan743599@gmail.com>
2026-03-03 20:47:24 +0600

    added navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
 
   return (
-    <>
-     
+    <div className='bg-slate-100 min-h-screen'>
+     <Navbar></Navbar>
       
-    </>
+    </div>
   )
 }
 
